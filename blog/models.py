fbaec2347aedf4146566511252730f6655c0f6cf
Jacob Reilly <JSR938@student.bham.ac.uk>
2020-06-28 23:53:02 +0100

    Blog Detail Page Work

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
     author=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.TextField()
     written_date = models.DateTimeField(default=timezone.now)
