e75c46333a4a3c1b015b0a2153c57dda44542929
Jacob Reilly <JSR938@student.bham.ac.uk>
2020-06-29 15:15:36 +0100

    Disabling Debug Mode

--- bridging/settings.py
+++ bridging/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&mg0$xrxenyb_7_dl$f9hk)ojc!3dcr=d+*5!_!8wn%#y=#7mm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
